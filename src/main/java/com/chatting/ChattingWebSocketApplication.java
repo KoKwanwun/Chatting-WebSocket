1b348dadb488382e2fb545e9ccf7816ef8671eb1
KoKwanwun <dkfajflsla@naver.com>
2023-06-19 21:37:50 +0900

    💻[build] 프로젝트 빌드 추가

---
+++ src/main/java/com/chatting/ChattingWebSocketApplication.java
@@ -0,0 +1,13 @@
+package com.chatting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattingWebSocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChattingWebSocketApplication.class, args);
+	}
+
+}
