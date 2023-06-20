de44b3a552287706c9d95f1745913de4dcb39245
KoKwanwun <dkfajflsla@naver.com>
2023-06-21 00:16:42 +0900

    🚀[feat] Response 추가

---
+++ src/main/java/com/chatting/domain/Response.java
@@ -0,0 +1,27 @@
+package com.chatting.domain;
+
+import lombok.*;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Response<T> {
+    private String resultCode;
+    private T result;
+
+    public static <T> Response<T> success(T result) {
+        return Response.<T>builder()
+                .resultCode("SUCCESS")
+                .result(result)
+                .build();
+    }
+
+    public static <T> Response<T> error(T result) {
+        return Response.<T>builder()
+                .resultCode("ERROR")
+                .result(result)
+                .build();
+    }
+}
