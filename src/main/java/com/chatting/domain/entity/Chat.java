e73106c50b472947b4d98ec821042a1e57748b9f
KoKwanwun <dkfajflsla@naver.com>
2023-06-20 22:43:24 +0900

    🚀[feat] Entity 추가 - User : 유저 - Chat : 채팅 - ChatRoom : 채팅방 - ChatParticipant : 채팅방참가현황

---
+++ src/main/java/com/chatting/domain/entity/Chat.java
@@ -0,0 +1,32 @@
+package com.chatting.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Chat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+    private String created_date;
+    private String deleted_date;
+
+    // 유저 아이디, 채팅방 아이디
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chat_room_id")
+    private ChatRoom chatRoom;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
---
+++ src/main/java/com/chatting/domain/entity/ChatParticipant.java
@@ -0,0 +1,28 @@
+package com.chatting.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChatParticipant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 유저 아이디, 채팅방 아이디
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chat_room_id")
+    private ChatRoom chatRoom;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
